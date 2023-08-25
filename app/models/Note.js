d5579e613120827c3b4b91f72d47493970f6486e
Austin <austinjperry22@gmail.com>
2023-08-25 10:32:20 -0600

    date

--- app/models/Note.js
+++ app/models/Note.js
@@ -1,11 +1,14 @@
 export class Note {
     constructor(data) {
         this.name = data.name
+        this.color = data.color
+        this.title = data.title || 'Title'
+        this.createdTime = new Date()
     }
 
     get template() {
         return `
-    my name is ${this.name}
+    ${this.title} my name is ${this.name}, date = ${this.createdTime.toDateString()} ${this.createdTime.toLocaleTimeString([], { hour: "2-digit", minute: '2-digit' })} 
     `
     }
 }
