390f548b3d1255c7a35bfff67dc9015eb28091d2
Austin <austinjperry22@gmail.com>
2023-08-25 15:29:03 -0600

    offcanvas

--- app/views/NotesView.js
+++ app/views/NotesView.js
@@ -1,27 +1,24 @@
 export const NotesView = /*html*/`
 <section class="row justify-content-between">
-      <!-- SIDEBAR WITH NOTES -->
-      <div class="col-md-2 justify-content-between bg-primary height100">
-        <!-- HEADER -->
-        <section class="row justify-content-between">
-          <h4 class="col-6">Jots</h4>
-          <h4 class="col-6 text-end" id="notesNumber">##</h4>
-        </section>
-        <section class="row" id="notesList">
-          <!-- notes template -->
-          <!-- <div class="col-12">
-            <h4>HTML</h4>
-          </div> -->
-        </section>
+<div class="offcanvas offcanvas-start bg-primary" tabindex="-1" id="offcanvas" aria-controls="offcanvas">
+<div class="offcanvas-header">
+  <h3 class="offcanvas-title">Jots: <span id="notesNumber">0</span></h3>
+  <button type="btn btn-close col-2" data-bs-dismiss="offcanvas" aria-label="Close">X</button>
+</div>
+<div class="offcanvas-body row">
+  <div class="col-12 justify-content-between  height100">
+    <section class="row" id="notesList">
+    </section>
 
-        <!-- New Note -->
-        <form class="row justify-content-evenly pt-3" onsubmit="app.NotesController.createNote()">
-          <label for="title">NEW NOTE</label>
-          <input class="col-2" type="color" name="color">
-          <input class="col-7" type="text" placeholder="Note Title"  required minlength="3" maxlength="15" name="title">
-          <button class="col-3" type="submit">Add</button>
-        </form>
-      </div>
+    <form class="row justify-content-evenly pt-3" onsubmit="app.NotesController.createNote()">
+      <label for="title">NEW NOTE</label>
+      <input class="col-2" type="color" name="color">
+      <input class="col-7" type="text" placeholder="Note Title" required minlength="3" maxlength="15" name="title">
+      <button class="col-3" type="submit">Add</button>
+    </form>
+  </div>
+</div>
+</div>
 
 
 
